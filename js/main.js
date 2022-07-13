c18c5b36f2306282a3d20d2a54e368b39cc6614c
sjurate <rasaujuratei@gmail.com>
2022-07-13 21:19:11 +0300

    js-intro-virtuve

---
+++ js/main.js
@@ -0,0 +1,20 @@
+console.clear();
+
+import { Kaitlente } from "./Kaitlente.js";
+
+const kaitlenciuKiekis = 5; 
+
+//const h1DOM = document.querySelector('h1');
+
+const tittleDOM = document.queryCommandValue('.tittle');
+
+// skyleDOM = document.queryCommandValue('.kaitlente .skyle:nth-child(3)) ;
+// skylesDOM = document.queryCommandValueAll('.kaitlente .skyle) ;
+
+
+
+
+for (let i = 1; i <= 1; i++) {
+    const irenginys = new Kaitlente(i);
+
+}
