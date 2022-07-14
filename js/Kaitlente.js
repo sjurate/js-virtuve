7ff9ecd5aa63ccf400e0b87d0bc8984c06fe891f
sjurate <rasaujuratei@gmail.com>
2022-07-14 13:45:57 +0300

    update

--- js/Kaitlente.js
+++ js/Kaitlente.js
@@ -1,9 +1,27 @@
 class Kaitlente {
+
     constructor(skyliuKiekis) {
         this.size = skyliuKiekis; 
         console.log(this);
     } 
 
+    render () {
+        const mainDOM = document.querySelector('main');
+        const HTML = `<div class="kaitlente" style="width: 362px;">
+            <div class="top">
+                <div class="skyle karsta"></div>
+                <div class="skyle"></div>
+                <div class="skyle"></div>
+            </div>
+            <div class="bottom">
+                <div class="jungiklis ijungtas"></div>
+                <div class="jungiklis"></div>
+                <div class="jungiklis"></div>
+            </div>
+        </div>`;
+
+        mainDOM.innerHTML += HTML ; 
+    }
 }
 
 export { Kaitlente } ;
--- js/main.js
+++ js/main.js
@@ -6,15 +6,41 @@ const kaitlenciuKiekis = 5;
 
 //const h1DOM = document.querySelector('h1');
 
-const tittleDOM = document.queryCommandValue('.tittle');
+const titleDOM = document.querySelector('.title');
+
+//console.log(titleDOM);
+//console.log(titleDOM.innerText);
+//console.log(titleDOM.textContent);
+
+titleDOM.innerText = `Virtuve (${kaitlenciuKiekis})`;
+
+// titleDOM.classList[0];  
+// titleDOM.classList.add('bold');  - 'bold' be tasko priekyje, nes neiesko, zino, kad tokia yra. 
+
+
+//KOMENTARAS - kad h1 taptu BOLD, kai skyliu kiekis lyginis
+// if (kaitleciuKiekis % 2 === 0) {
+//    titleDOM.classList.add('bold');
+//}
+
+// KOMENTARAS - kad h1 taptu RED, kai bent viena skyle karsta
+// if (document.querySelector('.karsta')) {
+// titleDOM.classList.add('red', 'sparkle');
+// titleDOM.classList.remove('red');
+// titleDOM.style.fontSize = '40px'; 
+// titleDOM.style['font-size'] = '40px';
+// }
+
 
 // skyleDOM = document.queryCommandValue('.kaitlente .skyle:nth-child(3)) ;
 // skylesDOM = document.queryCommandValueAll('.kaitlente .skyle) ;
 
+document.querySelector('main').innerText = '';
 
+console.log([document.querySelector('main')]);
 
-
-for (let i = 1; i <= 1; i++) {
+for (let i = 1; i <= kaitlenciuKiekis; i++) {
     const irenginys = new Kaitlente(i);
+    irenginys.render();
 
 }
